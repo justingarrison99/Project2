5060f1cba4f86e2a01043061cda3d63e2c851d6a
garrisonjd <garrisonjd@g.cofc.edu>
2020-03-02 22:01:06 -0500

    better commit#3

--- Project2main.py
+++ Project2main.py
@@ -1,5 +1,6 @@
 import sys
 import random
+import math
 
 
 class Job:
@@ -9,16 +10,42 @@ class Job:
         self.mem = random.randrange(min_mem, max_mem)
 
     def __repr__(self):
-        return "Job: [name: {0}, time: {1}, mem: {2}]".format(self.job, self.runT, self.mem)
+        return "[{0}, time: {1}, mem: {2}]".format(self.job, self.runT, self.mem)
+
+    def allocatePages(self):
+        temp = math.ceil(self.mem / pagesize)  # gets the number of pages a job needs
+
+        for k in range(len(pagetable)):         # loop through entire pagetable
+            if pagetable[k] == '.':             # find the first free space
+                if temp + k <= len(pagetable):  # make sure there's continous page space
+                    for j in range(k, temp + k):  # make sure there's continous free page space
+                        if pagetable[j] != '.':
+                            break                # if there's not continous free space break out and try the next
+                                                 # position
+
+                    self.setPage(k, k + temp)  # if there is continous free space,
+                                                # then set those pages to the corresponding job number
+                    return True
+        return False
+
+    def setPage(self, start, end):  # sets the pages to the corresponding job number
+        for i in range(start, end):
+            pagetable[i] = self.job
 
 
 cpumem = None
 pagesize = None
 jobnum = None
+pagetable = []
+jobQ = []
 readyQ = []
+runningQ = []
+
+
 # 1. size of computer memory, 2. size of a page, 3. # of jobs,
 # 4. min run time, 5. max run time  6. min memory, 7. max memory
 if __name__ == '__main__':
+
     cpumem = int(sys.argv[1])
     if cpumem % int(sys.argv[2]) == 0:
         pagesize = int(sys.argv[2])
@@ -32,15 +59,22 @@ if __name__ == '__main__':
     minmem = int(sys.argv[6])
     maxmem = int(sys.argv[7])
 
+    pagetable = ['.'] * math.ceil((cpumem / pagesize))
+
     print("Simulator Parameters: \n Memory Size: {0} \n Page Size: {1} \n Random Seed: ".format(cpumem, pagesize))
     print(" Number of jobs: {0} \n Runtime (min - max): {1}-{2} \n Memory (min - max): {3}-{4}".format(jobnum, minrun,
                                                                                                        maxrun, minmem,
                                                                                                        maxmem))
 
     for x in range(jobnum):
-        readyQ.append(Job(("Job " + str(x)), minrun, maxrun, minmem, maxmem))
+        jobQ.append(Job((str(x)), minrun, maxrun, minmem, maxmem))
+        if jobQ[x].allocatePages():
+            readyQ.append(jobQ[x])
+
+    print(readyQ)
+    print(pagetable)
 
     print("\nJob Queue: ")
-    print(" / Job #  / Runtime  / Memory / ")
-    for i in range(len(readyQ)):
-        print("| {0}        {1}      {2}".format(readyQ[i].job, readyQ[i].runT, readyQ[i].mem))
+    print(" / Job #    /  Runtime /   Memory / ")
+    for i in range(len(jobQ)):
+        print(jobQ[i])
